ad374866a835a9986ed77380ba60679e93cb8828
dennisvg111 <dennisvg111@gmail.com>
2018-11-02 13:07:48 +0100

    initial commit

---
+++ DG.Sound/Program.cs
@@ -0,0 +1,73 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DG.Sound
+{
+    class Program
+    {
+        private static int fftLength = 1024; // NAudio fft wants powers of two!
+        private static SpectrumAnalyzer analyzer = new SpectrumAnalyzer();
+        private static WaveStream wave;
+        private static long totalBytes;
+
+        // There might be a sample aggregator in NAudio somewhere but I made a variation for my needs
+        private static SampleAggregator sampleAggregator = new SampleAggregator(fftLength);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            sampleAggregator.FftCalculated += new EventHandler<FftEventArgs>(FftCalculated);
+            sampleAggregator.PerformFFT = true;
+
+            // Here you decide what you want to use as the waveIn.
+            // There are many options in NAudio and you can use other streams/files.
+            // Note that the code varies for each different source.
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            wave = new WaveChannel32(new Mp3FileReader(ofd.FileName));
+            string totalTime = wave.TotalTime.ToString();
+            var x = wave.Length;
+            byte[] buffer = new byte[fftLength];
+            int offset = 0;
+            int bytes = 1;
+            while (wave.CanRead && bytes > 0 && wave.CurrentTime <= wave.TotalTime)
+            {
+                bytes = wave.Read(buffer, 0, fftLength);
+                totalBytes += bytes;
+                offset += bytes;
+                WaveInEventArgs e = new WaveInEventArgs(buffer, bytes);
+                OnDataAvailable(null, e, wave.CurrentTime);
+            }
+        }
+
+        static void OnDataAvailable(object sender, WaveInEventArgs e, TimeSpan currentTime)
+        {
+            analyzer.SetTime(currentTime);
+            byte[] buffer = e.Buffer;
+            int bytesRecorded = e.BytesRecorded;
+            int bufferIncrement = wave.WaveFormat.BlockAlign;
+
+            for (int index = 0; index < bytesRecorded; index += bufferIncrement)
+            {
+                float sample32 = BitConverter.ToSingle(buffer, index);
+                sampleAggregator.Add(sample32);
+            }
+        }
+
+        static void FftCalculated(object sender, FftEventArgs e)
+        {
+            // Do something with e.result!
+            analyzer.Update(e.Result);
+        }
+    }
+}
---
+++ DG.Sound/SpectrumAnalyzer.cs
@@ -0,0 +1,73 @@
+﻿using NAudio.Dsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DG.Sound
+{
+    class SpectrumAnalyzer
+    {
+        private int bins = 0;
+        private TimeSpan currentTime = TimeSpan.Zero;
+
+        public void Update(Complex[] fftResults)
+        {
+            if (fftResults.Length / 2 != bins)
+            {
+                bins = fftResults.Length / 2;
+            }
+
+            int binsEachCharacter = bins / Console.WindowWidth;
+
+            for (int n = 0; n < fftResults.Length / 2; n += binsEachCharacter)
+            {
+                // averaging out bins
+                double yPos = 0;
+                for (int b = 0; b < 1; b++)
+                {
+                    yPos += GetYPosLog(fftResults[n + b]);
+                }
+                AddResult(n / binsEachCharacter, yPos / 1);
+            }
+        }
+
+        private double GetYPosLog(Complex c)
+        {
+            // not entirely sure whether the multiplier should be 10 or 20 in this case.
+            // going with 10 from here http://stackoverflow.com/a/10636698/7532
+            double intensityDB = 10 * Math.Log10(Math.Sqrt(c.X * c.X + c.Y * c.Y));
+            double minDB = -90;
+            if (intensityDB < minDB) intensityDB = minDB;
+            double percent = intensityDB / minDB;
+            // we want 0dB to be at the top (i.e. yPos = 0)
+            double yPos = percent * Console.WindowHeight;
+            return yPos;
+        }
+
+        private void AddResult(int index, double power)
+        {
+            for (int i = 0; i < power; i++)
+            {
+                ConsoleLib.ConsoleWriter.WriteCharacterAt(index, i, '#', ConsoleColor.Black);
+            }
+            for (int i = (int)power; i < Console.WindowHeight; i++)
+            {
+                ConsoleLib.ConsoleWriter.WriteCharacterAt(index, i, '#', ConsoleColor.Green);
+            }
+            string time = currentTime.ToString();
+            int timeChar = 0;
+            foreach (var @char in time)
+            {
+                ConsoleLib.ConsoleWriter.WriteCharacterAt(timeChar, 0, @char, ConsoleColor.White);
+                timeChar++;
+            }
+        }
+
+        public void SetTime(TimeSpan currentTime)
+        {
+            this.currentTime = currentTime;
+        }
+    }
+}
